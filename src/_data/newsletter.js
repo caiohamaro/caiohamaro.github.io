1673e13fe0342c3f08f4f44c4bd9a995227afc10
Caio Amaro <caio.amaro@gmail.com>
2025-07-08 23:59:26 -0300

    atualização de newsletter

--- src/_data/newsletter.js
+++ src/_data/newsletter.js
@@ -1,13 +1,1 @@
-const axios = require('axios')
 
-const url = 'https://api.jdsteinbach.com/.netlify/functions/newsletter'
-
-const getNewsletter = () => axios.get(url)
-  .then(({ data }) => {
-    return data
-  })
-  .catch(error => {
-    console.error(error)
-  })
-
-module.exports = getNewsletter()
